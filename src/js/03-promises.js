7f52fc4641e6a360dc43c1c5d454281b9b61db4a
Roman-Lviv <kituraroman@gmail.com>
2023-10-15 14:27:10 +0300

    git add .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,32 @@
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', async function (event) {
   event.preventDefault();
 
-  let delay = Number(this.querySelector('[name="delay"]').value);
+  const delay = Number(this.querySelector('[name="delay"]').value);
   const step = Number(this.querySelector('[name="step"]').value);
   const amount = Number(this.querySelector('[name="amount"]').value);
 
   for (let position = 1; position <= amount; position++) {
     try {
       const result = await createPromise(position, delay);
-      console.log(
-        `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
-      );
+      notiflix.Notify.Success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
     } catch (error) {
-      console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+      notiflix.Notify.Failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
     }
 
     delay += step;
