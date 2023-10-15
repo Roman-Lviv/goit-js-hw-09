53825dcb674d1c358ffda13ff49f58cd595f4c0d
Roman-Lviv <135068902+Roman-Lviv@users.noreply.github.com>
2023-10-15 10:30:30 +0000

    Deploying to gh-pages from @ Roman-Lviv/goit-js-hw-09@9553fff319c9799463edd22dc10a2f1341784c46 🚀

---
+++ 01-color-switcher.c69f44c4.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),d=document.body;e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),a.addEventListener("click",(function(){e.disabled=!1,a.disabled=!0,t&&clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.c69f44c4.js.map
---
+++ 01-color-switcher.c7d42632.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.body;let a;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,a&&clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.c7d42632.js.map
